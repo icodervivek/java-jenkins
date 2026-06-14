dceb8ef1b4c155eaf47282e2569c06202570f4fb
Vivek <vivekpramanikbundu@gmail.com>
2026-06-14 11:36:49 +0530

    feat: add Main file

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello There!");
+        System.out.println("Date: " + new Date());
+    }
+}
